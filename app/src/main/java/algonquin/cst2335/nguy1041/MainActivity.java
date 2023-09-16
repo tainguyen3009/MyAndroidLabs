77579fb6a9a562968e487a894c564389468e0ad6
Tai Nguyen <phepha99@gmail.com>
2023-09-15 22:27:45 -0400

    fifth commit: save undone work

--- app/src/main/java/algonquin/cst2335/nguy1041/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/nguy1041/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.nguy1041;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    TextView mytext = findViewById(R.id.textview);
+    Button b = findViewById(R.id.Button);
+
+    EditText editText = findViewById(R.id.editext);
+
+    public void onClick(View v){
+
     }
 }
