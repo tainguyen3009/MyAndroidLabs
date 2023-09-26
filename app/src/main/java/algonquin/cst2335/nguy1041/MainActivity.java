12ace233cacedb1ffaeab9ce7a491f7efc0304bf
Tai Nguyen <phepha99@gmail.com>
2023-09-25 22:25:05 -0400

    W3: Finished Phone Version of Lab3

--- app/src/main/java/algonquin/cst2335/nguy1041/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/nguy1041/MainActivity.java
@@ -2,13 +2,42 @@ package algonquin.cst2335.nguy1041;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgView;
+    Switch sw;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // spin function
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
+        sw.setOnCheckedChangeListener((btn, ischecked) -> {
+            if (ischecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+        });
+
     }
 }
